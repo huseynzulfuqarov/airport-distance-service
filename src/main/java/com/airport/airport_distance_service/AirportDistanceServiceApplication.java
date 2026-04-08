a318ba68cca824392961c9a51e159379ac6c4e98
Huseyn Zulfugarov <130462640+huseynzulfuqarov@users.noreply.github.com>
2026-04-08 15:34:00 +0400

    initial project setup

---
+++ src/main/java/com/airport/airport_distance_service/AirportDistanceServiceApplication.java
@@ -0,0 +1,13 @@
+package com.airport.airport_distance_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirportDistanceServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AirportDistanceServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/airport/airport_distance_service/AirportDistanceServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.airport.airport_distance_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirportDistanceServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
